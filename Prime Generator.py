03a8ae1863bff64ba96689221b667aeb126704aa
Andy Wang <34005234+wangazz@users.noreply.github.com>
2018-11-26 13:51:40 +0000

    Add files via upload
    Adds Lucas-Lehmer primality testing for Mersenne primes. Speed improvements.

--- Prime Generator.py
+++ Prime Generator.py
@@ -1,44 +1,56 @@
+import math
 import time
 
 def is_prime(x):
     d = 2 # divisor
-    while d <= x//2:
-        if x/d == x//d:
+    while d <= int(math.sqrt(x)):
+        if x%d == 0: # remainder of x/d
             return False
-        elif d == x//2:
+        elif d >= int(math.sqrt(x)):
             return True
         else:
             d += 1
 
 def generate_primes(n):
     start = time.time()
-    i = 0 # primes generated
-    x = 2 # first integer tested
-
+    print(2)
+    print(3)
+    i = 2 # primes generated
+    x = 5 # first integer tested
     while i < n:
         if is_prime(x) is True:
             print(x)
-            i += 1 # 1 prime created
-            x += 1 # test next integer
+            i += 1
+            x += 2
         else:
-            x += 1
+            x += 2
 
     end = time.time()
     time_taken = end - start
     print(str(n) + " primes were generated in " + str(time_taken) + "s.")
 
+def lucas_lehmer(p): # Lucas-Lehmer test if 2^p - 1 is prime
+    s = 4
+    M = 2**p - 1
+    for x in range(0, p-2):
+        s = (s**2 - 2)%M
+    if s == 0:
+        return True
+    else:
+        return False
+    
 def generate_mersenne(n):
     start = time.time()
-    i = 0 # primes generated
-    x = 2 # first integer tested
-
+    print(3)
+    i = 1 # primes generated
+    p = 3 # first exponent tested
     while i < n:
-        if is_prime(2**x - 1) is True:
-            print(2**x - 1)
+        if lucas_lehmer(p) is True:
+            print(2**p - 1)
             i += 1 # 1 prime created
-            x += 1 # test next integer
+            p += 1 # test next exponent
         else:
-            x += 1
+            p += 1
 
     end = time.time()
     time_taken = end - start
