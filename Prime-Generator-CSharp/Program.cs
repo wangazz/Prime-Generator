88d7278c4c74e97219fd1afd9e072e03cf17045d
Andy Wang <andywang.16@gmail.com>
2019-11-27 21:34:37 +0000

    Added C# files

---
+++ Prime-Generator-CSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prime_Generator_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
